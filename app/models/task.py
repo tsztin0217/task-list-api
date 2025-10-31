86d366d1b1abf557967e644e319389639a5ba7a8
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-10-30 21:54:52 -0400

    Implement task creation endpoint and define Task model with return_dict method

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,19 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+from sqlalchemy import DateTime
+from typing import Optional
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable=True)
+
+    def return_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at if self.completed_at else None
+        }
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from ..db import db
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+
+
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body.get("completed_at")
+
+    new_task = Task(title=title, description=description, completed_at=completed_at)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = new_task.return_dict()
+
+    return response, 201
