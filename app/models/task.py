e80aa931c2b4502cda17f5725d7fafe6b0cd092f
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-11-01 03:32:17 -0400

    Refactor Task model to update completed_at column type and add comments from_dict and to_dict methods for self-reminders

--- app/models/task.py
+++ app/models/task.py
@@ -8,9 +8,29 @@ class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
-    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable=True)
+    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
 
-    # for the response when we get or create a task
+    # for creating/updating a Task from a dictionary from a request body
+    # which came in json format
+    # then converted to a dictionary by request.get_json()
+    # and here we create a Task object from that dictionary
+    # so that we can add it to the database
+    # flow: from request body in json --> from_dict() --> to Task object
+    @classmethod
+    def from_dict(cls, task_dict):
+        is_complete = task_dict.get("is_complete", False)
+        completed_at = datetime.now() if is_complete else None
+        
+        return cls(
+            title=task_dict["title"],
+            description=task_dict["description"],
+            completed_at=completed_at
+        )
+
+    # to return a response with the new info including newlyy generated id 
+    # after we get or create a task record in database
+    # return a dictionary representation of the Task object
+    # flow: from database --> Task object --> to_dict() --> to json response
     def to_dict(self):
         return {
             "id": self.id,
@@ -19,14 +39,3 @@ class Task(db.Model):
             "is_complete": self.completed_at is not None
         }
     
-    # for creating a Task from a dictionary from a request body
-    @classmethod
-    def from_dict(cls, task_dict):
-        is_complete = task_dict.get("is_complete", False)
-        completed_at = datetime.now(datetime) if is_complete else None
-        
-        return cls(
-            title=task_dict["title"],
-            description=task_dict["description"],
-            completed_at=completed_at
-        )
