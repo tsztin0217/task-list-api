08d7f94c7cd3c6b08e520ac9e8c61e150c9dacb4
Tsz Tin Vivian Soo <tsztin0217@gmail.com>
2025-12-18 00:39:43 -0500

    Add CORS support to the Flask app and update requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .db import db, migrate
 from .models import task, goal
 from .routes.task_routes import bp as tasks_bp 
@@ -7,6 +8,8 @@ import os
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
